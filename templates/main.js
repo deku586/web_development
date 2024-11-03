b3e860dad25ea452706cc025d2c9f6790aa2763a
deku586 <wendreumonteiro@gmail.com>
2024-11-03 20:51:07 -0300

    Melhorado o menu de navegaçao, as fotos dos procedimentos mudam após um periodo de tempo(em teste), layout mais responsivo, pequenas na aparencia do site

--- templates/main.js
+++ templates/main.js
@@ -1,19 +1,47 @@
-// Função para inicializar o mapa
 function initMap() {
-    // Coordenadas da clínica (exemplo: São Paulo)
-    //
-    const localizacaoClinica = { lat: -23.60344335895204, lng: -46.52975984700882 }; // Substitua com as coordenadas corretas
 
-    // Cria um mapa centralizado nas coordenadas
+    const localizacaoClinica = { lat: -23.60344335895204, lng: -46.52975984700882 }; 
+
+    
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15, // Nível de zoom ajustável
+        zoom: 15, 
         center: localizacaoClinica,
     });
 
-    // Marca o local da clínica no mapa
+   
     const marker = new google.maps.Marker({
         position: localizacaoClinica,
         map: map,
         title: "Nossa Clínica",
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const Pcorporal = ["../static/Crio01.jpeg", "../static/mae.png", "../static/profilePicture.jpeg"];
+    const Pfacial = ["../static/mae.png", "../static/mae2.png", "../static/mae3.png"];
+    const Pgluteos = [];
+    const Phidrolipo = [];
+    const Pcrio = [];
+   
+
+    let index1 = 0;
+    let index2 = 0;
+
+
+    function changeImage1() {
+        const imgElement = document.getElementById("carousel-corporal");
+        index1 = (index1 + 1) % Pcorporal.length;
+        imgElement.src = Pcorporal[index1];
+    }
+
+    function changeImage2() {
+        const imgElement = document.getElementById("carousel-facial");
+        index2 = (index2 + 1) % Pfacial.length;
+        imgElement.src = Pfacial[index2];
+    }
+
+    
+    setInterval(changeImage1, 5000);
+    setInterval(changeImage2, 5000);
+});
