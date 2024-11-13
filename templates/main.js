6805160a2fbd97a470ad5bc9768bdff2ab951a24
deku586 <wendreumonteiro@gmail.com>
2024-11-13 09:40:46 -0300

    Haire, SEKIRO!

--- templates/main.js
+++ templates/main.js
@@ -15,33 +15,3 @@ function initMap() {
         title: "Nossa Clínica",
     });
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    
-    const Pcorporal = ["../static/Crio01.jpeg", "../static/mae.png", "../static/profilePicture.jpeg"];
-    const Pfacial = ["../static/mae.png", "../static/mae2.png", "../static/mae3.png"];
-    const Pgluteos = [];
-    const Phidrolipo = [];
-    const Pcrio = [];
-   
-
-    let index1 = 0;
-    let index2 = 0;
-
-
-    function changeImage1() {
-        const imgElement = document.getElementById("carousel-corporal");
-        index1 = (index1 + 1) % Pcorporal.length;
-        imgElement.src = Pcorporal[index1];
-    }
-
-    function changeImage2() {
-        const imgElement = document.getElementById("carousel-facial");
-        index2 = (index2 + 1) % Pfacial.length;
-        imgElement.src = Pfacial[index2];
-    }
-
-    
-    setInterval(changeImage1, 5000);
-    setInterval(changeImage2, 5000);
-});
